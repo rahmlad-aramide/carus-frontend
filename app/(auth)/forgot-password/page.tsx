d8ecb4c6ca534f56349e2c2e89ff24ca4fb2144e
Olamide1705 <abdullateefnafisat@gmail.com>
2025-08-08 23:49:49 +0100

    Authentication UI and Form Handling

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [sent, setSent] = useState(false);
+
+  const handleSendLink = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Sending reset link to:", email);
+
+    setSent(true);
+  };
+
+  return (
+    <div className="flex min-h-screen">
+      {/* flex form and bg image */}
+      <div className="w-full md:w-2/3 flex flex-col py-10 bg-white">
+        {/* Logo */}
+        <div className="px-3 md:px-5 mb-15">
+          <Image
+            aria-hidden
+            src="/logo.png"
+            alt="Carus Logo"
+            width={117}
+            height={32}
+            className="w-[117px] h-[32px]"
+          />
+        </div>
+
+        <div className="flex-1 flex px-5 md:px-20">
+          <div className="w-full max-w-sm">
+            <h2 className="text-[28px] font-bold">Forgot Password</h2>
+            <p className="text-base text-[#494949] mb-8">
+              Enter your registered Email or Phone Number to reset your password
+            </p>
+
+            {sent ? (
+              <p className="text-green-600 text-center">
+                A reset link has been sent to your email.
+              </p>
+            ) : (
+              <form onSubmit={handleSendLink}>
+                <label htmlFor="email" className="block text-base mb-2">
+                  Email or Phone Number
+                </label>
+                <Input
+                  type="email"
+                  className="rounded-[10px] w-full py-6 text-sm bg-[#F3F3F3] border-none mb-10"
+                  placeholder="example@mail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+
+                <Button
+                  type="submit"
+                  className="rounded-[10px] w-full py-6 text-sm md:text-base font-bold"
+                >
+                  Send Reset Link
+                </Button>
+              </form>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden md:block w-[50%] relative overflow-hidden bg-gradient-to-t from-[#C7DCD2] via-[#E5EFEA] to-[#F9FBFA]">
+        <div className="absolute inset-0">
+          <Image
+            src="/wastecan1.svg"
+            alt="Waste Bin"
+            fill
+            className="object-contain object-right bg-inherit"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/(auth)/otp-verification/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function VerifyRegistrationOTP() {
+  const [otp, setOtp] = useState(["", "", "", ""]);
+  const inputsRef = useRef<Array<HTMLInputElement | null>>([]);
+
+  const handleChange = (value: string, index: number) => {
+    if (/^\d?$/.test(value)) { // Allow only one digit
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+
+      // Move to next input automatically
+      if (value && index < otp.length - 1) {
+        inputsRef.current[index + 1]?.focus();
+      }
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    if (e.key === "Backspace" && !otp[index] && index > 0) {
+      inputsRef.current[index - 1]?.focus();
+    }
+  };
+
+  const handleVerifyOTP = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const code = otp.join("");
+    console.log("Verifying OTP:", code);
+
+    // TODO: Call backend API
+  };
+
+  return (
+    <div className="max-w-sm mx-auto mt-10 p-6 border rounded-lg shadow-sm bg-white">
+      <h2 className="text-[28px] font-bold mb-4">Verification</h2>
+      <p className="text-base">
+        Enter the verification code sent to your email below.
+      </p>
+
+      <form onSubmit={handleVerifyOTP} className="space-y-6">
+        <div className="flex justify-center gap-3">
+          {otp.map((digit, index) => (
+            <input
+              key={index}
+             ref={(el) => {inputsRef.current[index] = el;}}
+              type="text"
+              inputMode="numeric"
+              maxLength={1}
+              value={digit}
+              onChange={(e) => handleChange(e.target.value, index)}
+              onKeyDown={(e) => handleKeyDown(e, index)}
+              className="rounded-[10px] w-[48px] h-[48px] py-1 text-[24px] bg-[#CCE1D7] text-center border focus:outline-none"
+            />
+          ))}
+        </div>
+          <p className="mt-7 mb-7 text-base text-centr">Code expires in 10mins</p>
+        <Button
+          type="submit"
+       className="rounded-[10px] w-full py-6 text-sm md:text-base font-bold"
+           disabled={otp.some((digit) => !digit)} >
+          Verify
+        </Button>
+      </form>
+    </div>
+  );
+}
