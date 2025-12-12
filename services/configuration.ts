5d55466794f00287c9ac21c3659befdbdc370074
Olamide1705 <abdullateefnafisat@gmail.com>
2025-12-12 03:12:15 +0100

    feat: implement backend endpoints for redeeming points (airtime and cash)

--- services/configuration.ts
+++ services/configuration.ts
@@ -1,18 +1,14 @@
 import http from "@/services/http";
-import {
-  ConfigurationsResponse,
-  LocationConfigResponse,
-  PointToNairaResponse,
-} from "@/types/configuration";
+import { PointToNairaResponse } from "@/types/configuration";
 
-export async function getConfigurations(): Promise<ConfigurationsResponse> {
-  return (await http.get("/configurations")).data;
-}
+// export async function getConfigurations(): Promise<ConfigurationsResponse> {
+//   return (await http.get("/configurations")).data;
+// }
 
 export async function getPointToNairaRate(): Promise<PointToNairaResponse> {
   return (await http.get("/configurations/point-to-naira")).data;
 }
 
-export async function getLocationConfig(): Promise<LocationConfigResponse> {
-  return (await http.get("/configurations/location")).data;
-}
+// export async function getLocationConfig(): Promise<LocationConfigResponse> {
+//   return (await http.get("/configurations/location")).data;
+// }
